1430529a447386b3ab1a9458adc52e6820794c64
tangerg <tangerg@qq.com>
2024-10-05 11:40:42 +0800

    feat: archive无用代码(core)

--- core/message/message.go
+++
@@ -1,9 +0,0 @@
-package message
-
-type Message interface {
-	Payload() []byte
-	SetPayload(payload []byte) Message
-	Headers() Headers
-	SetHeaders(headers Headers) Message
-	Unmarshal(v any) error
-}
