11259b32dbb6c2159e97107a0417a21aff696744
tangerg <tangerg@qq.com>
2024-12-24 13:17:04 +0800

    feat: pkg slices Split

--- pkg/slices/slices.go
+++ pkg/slices/slices.go
@@ -15,3 +15,43 @@ func ExpandToFit[T any](slice []T, index int) []T {
 	copy(newSlice, slice)
 	return newSlice
 }
+
+// Split divides a slice into smaller slices of a specified size.
+//
+// This is a generic function that works with slices of any type.
+//
+// Parameters:
+//   - slice: The input slice to be divided.
+//   - size: The maximum size of each sub-slice. If size <= 0, the function returns
+//     a two-dimensional slice containing the original slice as the only element.
+//
+// Returns:
+//   - [][]T: A two-dimensional slice where each sub-slice has a maximum length of `size`.
+//     The last sub-slice may have fewer elements if the total number of elements
+//     in the input slice is not evenly divisible by `size`.
+//
+// Examples:
+//
+//	slice := []int{1, 2, 3, 4, 5, 6}
+//	result := Split(slice, 2)
+//	// result: [[1 2] [3 4] [5 6]]
+//
+//	slice := []string{"a", "b", "c", "d", "e"}
+//	result := Split(slice, 3)
+//	// result: [["a" "b" "c"] ["d" "e"]]
+func Split[T any](slice []T, size int) [][]T {
+	if size <= 0 {
+		return [][]T{slice}
+	}
+
+	var (
+		rv     [][]T
+		length = len(slice)
+	)
+
+	for i := 0; i < length; i += size {
+		rv = append(rv, slice[i:min(i+size, length)])
+	}
+
+	return rv
+}
