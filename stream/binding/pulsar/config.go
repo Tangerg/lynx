5bef1bd522deee919cdd9b5bf441f2df424b1099
tangerg <tangerg@qq.com>
2025-11-18 17:30:16 +0800

    del: stream

--- stream/binding/pulsar/config.go
+++
@@ -1,9 +0,0 @@
-package pulsar
-
-import "github.com/Tangerg/lynx/stream/binding"
-
-type Config struct {
-	URL       string            `json:"URL" yaml:"URL"`
-	Topic     string            `json:"Topic" yaml:"Topic"`
-	Direction binding.Direction `json:"Direction" yaml:"Direction"`
-}
